69ef4bad0f9923e38b8566a459090e1c76ad3793
Victor Thomaz <victorthomazrdc@gmail.com>
2024-04-22 08:26:33 -0300

    first commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export const Home = () => {
+
+    return (
+        <View style={StyleSheet.container}>
+            <View style={styles.header}>
+
+            </View>
+
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
